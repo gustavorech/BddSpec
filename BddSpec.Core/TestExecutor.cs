a27aac0aba1309de8a6817ca63146c15ec53967b
gustavorech <rech.gustavo@hotmail.com>
2020-03-04 13:53:32 -0300

    initializing the repo

---
+++ BddSpec.Core/TestExecutor.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace bddlike
+{
+	public class TestExecutorDescription
+	{
+		public List<TestExecutorDescription> Children { get; } = new List<TestExecutorDescription>();
+		public int PositionInStack { get; }
+		public int Level { get; }
+		public string Description { get; }
+		public TestContextType Type { get; }
+
+		public bool IsChildrenDiscovered { get; set; }
+		public bool HasExecutionError { get; set; }
+
+		public bool IsExecutionCompleted
+		{
+			get
+			{
+				if (HasExecutionError)
+					return true;
+
+				return IsChildrenDiscovered && Children.TrueForAll(c => c.IsExecutionCompleted);
+			}
+		}
+
+		public bool BranchHasExecutionError
+		{
+			get
+			{
+				return HasExecutionError || Children.Any(c => c.BranchHasExecutionError);
+			}
+		}
+
+		public TestExecutorDescription(TestContext context, int positionInStack, int level)
+		{
+			Description = context.Description;
+			Type = context.Type;
+			PositionInStack = positionInStack;
+			Level = level;
+		}
+
+		public void AddChild(TestContext context, int positionInStack)
+		{
+			Children.Add(new TestExecutorDescription(context, positionInStack, Level + 1));
+		}
+
+		public void Print()
+		{
+			if (Level > 0)
+				Console.Write(new string(' ', Level * 2));
+
+			if (BranchHasExecutionError)
+				Console.ForegroundColor = ConsoleColor.DarkRed;
+			else
+				Console.ForegroundColor = ConsoleColor.Green;
+
+			switch (Type)
+			{
+				case TestContextType.Context:
+					Console.Write("[When] ");
+					break;
+				case TestContextType.It:
+					Console.Write("[It] ");
+					break;
+				default:
+					break;
+			}
+
+			Console.Write(Description);
+			Console.WriteLine();
+
+			Children.ForEach(c => c.Print());
+		}
+	}
+
+	public class TestExecutor<T>
+		where T : BddLike, new()
+	{
+		private int executed = 0;
+		private List<TestExecutorDescription> root = new List<TestExecutorDescription>();
+
+		public void Execute()
+		{
+			Initialize();
+
+			if (root.Count == 0)
+				return;
+
+			Iterate();
+		}
+
+		private void Iterate()
+		{
+			while (!root.TrueForAll(c => c.IsExecutionCompleted))
+			{
+				T instance = new T();
+				instance.ConfigureTests();
+
+				Recursion(instance, root);
+			}
+		}
+
+		private void Recursion(T instance, List<TestExecutorDescription> listToExecute)
+		{
+			TestExecutorDescription currentTestExecutor = listToExecute.First(c => !c.IsExecutionCompleted);
+			TestContext currentTestContext = instance.testContexts[currentTestExecutor.PositionInStack];
+
+			int currentStackCount = instance.testContexts.Count;
+			SafeInvoke(currentTestContext, currentTestExecutor);
+
+			if (currentTestExecutor.HasExecutionError)
+				return;
+
+			if (!currentTestExecutor.IsChildrenDiscovered)
+			{
+				for (int i = currentStackCount; i < instance.testContexts.Count; i++)
+				{
+					TestContext childTestContext = instance.testContexts[i];
+					currentTestExecutor.AddChild(childTestContext, i);
+				}
+
+				currentTestExecutor.IsChildrenDiscovered = true;
+
+				if (currentTestExecutor.IsExecutionCompleted)
+					return;
+			}
+
+			Recursion(instance, currentTestExecutor.Children);
+		}
+
+		private void Initialize()
+		{
+			T instance = new T();
+
+			instance.ConfigureTests();
+
+			for (int i = executed; i < instance.testContexts.Count; i++)
+			{
+				TestContext testContext = instance.testContexts[i];
+				root.Add(new TestExecutorDescription(testContext, i, 1));
+			}
+		}
+
+		private void SafeInvoke(TestContext context, TestExecutorDescription executorDescription)
+		{
+			try
+			{
+				context.Action.Invoke();
+			}
+			catch
+			{
+				executorDescription.HasExecutionError = true;
+			}
+		}
+
+		public void Print()
+		{
+			Console.Write(typeof(T).Name + ":");
+			Console.WriteLine();
+
+			root.ForEach(c => c.Print());
+		}
+	}
+}
---
+++ BddSpec.Core/bddlike.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace bddlike
+{
+
+	public enum TestContextType
+	{
+		Before,
+		After,
+		Method,
+		Context,
+		It
+	}
+
+	public class TestContext
+	{
+		public string Description { get; }
+		public TestContextType Type { get; }
+		public Action Action { get; }
+
+		public TestContext(string description, TestContextType type, Action action)
+		{
+			Description = description;
+			Type = type;
+			Action = action;
+		}
+	}
+
+	public abstract partial class BddLike
+	{
+		public class BddLikeAction
+		{
+			private BddLike bddLike;
+			private string description;
+			private TestContextType type;
+
+			public Action Do
+			{
+				set
+				{
+					bddLike.testContexts.Add(new TestContext(description, type, value));
+				}
+			}
+
+			public BddLikeAction(BddLike bddLike, string description, TestContextType type)
+			{
+				this.bddLike = bddLike;
+				this.description = description;
+				this.type = type;
+			}
+		}
+	}
+
+	public abstract partial class BddLike
+	{
+		public List<TestContext> testContexts { get; } = new List<TestContext>();
+
+		public abstract void ConfigureTests();
+
+		public Action ConfigureContext
+		{
+			set
+			{
+				testContexts.Add(new TestContext(null, TestContextType.Before, value));
+			}
+		}
+
+		public void When(string description, Action action) =>
+			testContexts.Add(new TestContext(description, TestContextType.Context, action));
+
+		public void It(string description, Action action) =>
+			testContexts.Add(new TestContext(description, TestContextType.It, action));
+	}
+
+	public class ToTest
+	{
+		public string Name { get; set; }
+	}
+
+	public class ToTestTest : BddLike
+	{
+		public override void ConfigureTests()
+		{
+			ToTest toTest = new ToTest();
+
+			When("the name is Gustavo", () =>
+			{
+				toTest.Name = "Gustavo";
+
+				It("is Gustavo", () => Assert.AreEqual(toTest.Name, "Gustavo"));
+				It("first letter is G", () => Assert.AreEqual(toTest.Name[0], 'P'));
+
+				When("has surname Rech", () =>
+				{
+					toTest.Name += " Rech";
+
+					throw new Exception("oq aconteceu");
+
+					It("is Gustavo Rech", () => Assert.AreEqual(toTest.Name, "Gustavo Rech"));
+					It("first letter is G", () => Assert.AreEqual(toTest.Name[0], 'G'));;
+				});
+			});
+
+			When("the name is Pedro", () =>
+			{
+				toTest.Name = "Pedro";
+
+				It("is Pedro", () => Assert.AreEqual(toTest.Name, "Pedro"));
+				It("first letter is P", () => Assert.AreEqual(toTest.Name[0], 'P'));
+			});
+		}
+	}
+}
