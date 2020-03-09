197782226d44d15766755c589bf9a0a450c016b4
Gustavo Rech <rech.gustavo@hotmail.com>
2020-03-09 19:10:00 -0300

    Starting to add class selector

--- BddSpec.Core/Options/StartOptions.cs
+++ BddSpec.Core/Options/StartOptions.cs
@@ -11,6 +11,10 @@ namespace BddSpec.Core.Options
 - v[erbose] : print every step during the execution -- this disables paralel execution
 - s[ummary] : print every step only after end of execution";
 
+
+        [Value(0, MetaName = "SpecSelector", Required = false, HelpText = "Spec class to execute")]
+        public string SpecSelector { get; set; }
+
         [Option('v', "verbosity", Default = OptionVerbosityLevel.errors, HelpText = VERBOSITY_HELP_TEXT)]
         public OptionVerbosityLevel VerbosityLevel { get; set; }
 
