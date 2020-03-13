72ad2eefd4acfc4b70407d2c6b632a4c7f85d466
Gustavo Rech <rech.gustavo@hotmail.com>
2020-03-13 19:08:13 -0300

    Add a project that references BddSpec by nuget

---
+++ BddSpec.TestFromNuget/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BddSpec.TestFromNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BddSpecManager.Execute(args);
+        }
+    }
+}
---
+++ BddSpec.TestFromNuget/ToTest.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+
+namespace BddSpec.ConsoleTest
+{
+    public class ToTest
+    {
+        public string Name { get; set; }
+    }
+
+    public class ToTestTest : SpecClass
+    {
+        public override void SetupSpecs()
+        {
+            ToTest toTest = new ToTest();
+
+            When("the name is Gustavo", () =>
+            {
+                toTest.Name = "Gustavo";
+
+                It("is Gustavo", () => toTest.Name.Should().Be("Gustavo"));
+                It("first letter is G", () => toTest.Name[0].Should().Be('G'));
+
+                When("has surname Rech", () =>
+                {
+                    toTest.Name += " Rech";
+
+                    //throw new Exception("oq aconteceu");
+
+                    It("is Gustavo Rech", () => toTest.Name.Should().Be("Gustavo Rech"));
+                    It("first letter is G", () => toTest.Name[0].Should().Be('G'));
+                });
+            });
+
+            When("the name is Pedro", () =>
+            {
+                //After = () => throw new Exception("wtf is going on?");
+                toTest.Name = "Pedro";
+
+                It("is Pedro", () => toTest.Name.Should().Be("Pedro"));
+                It("first letter is P", () => toTest.Name[0].Should().Be('P'));
+            });
+        }
+    }
+}
