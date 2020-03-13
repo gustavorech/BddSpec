12f1ab585ad497db01bf1c51391b0d9297d2a412
Gustavo Rech <rech.gustavo@hotmail.com>
2020-03-13 00:52:26 -0300

    Improve options text

--- BddSpec/Options/StartOptions.cs
+++ BddSpec/Options/StartOptions.cs
@@ -5,23 +5,29 @@ namespace BddSpec.Options
 {
     public class StartOptions
     {
+        const string FILTER_HELP_TEXT =
+@"Filter to execute a subset of the spec suite
+- executes only one spec class. ex.: NameOfClassSpec
+- executes a specific line of a spec class. ex.: NameOfClassSpec:120
+- executes all spec from one namespace. ex.: BddSpec.Core.%
+- any filter utilizing '%' for anything in between. ex.: %.Domain.%Client";
+
         const string VERBOSITY_HELP_TEXT =
-@"set the verbosity of the output
-- e[rrors]  : only show the errors at the end of execution
+@"Set the verbosity of the output
+- e[rrors]  : only show the errors (if have) at the end of execution
 - v[erbose] : print every step during the execution -- this disables paralel execution
-- s[ummary] : print every step only after end of execution";
+- s[ummary] : print a summary (verbose) after the end of execution";
 
-
-        [Value(0, MetaName = "SpecSelector", Required = false, HelpText = "Spec class to execute")]
+        [Value(0, MetaName = "Filter", Required = false, HelpText = FILTER_HELP_TEXT)]
         public string SpecSelector { get; set; }
 
         [Option('v', "verbosity", Default = OptionVerbosityLevel.errors, HelpText = VERBOSITY_HELP_TEXT)]
         public OptionVerbosityLevel VerbosityLevel { get; set; }
 
-        [Option('l', "show-line", Default = false, HelpText = "Shows the execution line after the step")]
+        [Option('l', "show-line", Default = false, HelpText = "Shows the execution line")]
         public bool ShowLine { get; set; }
 
-        [Option('t', "show-time", Default = false, HelpText = "Shows the execution time after the step")]
+        [Option('t', "show-time", Default = false, HelpText = "Shows the time spent on every step")]
         public bool ShowTime { get; set; }
 
         [Option('a', "async", Default = false, HelpText = "Executes spec classes asynchronous")]
